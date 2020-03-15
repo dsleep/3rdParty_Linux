f390c718e453015a50eb37032c22d538b8645c50
David Sleeper <david.sleeper@gmail.com>
2020-03-14 22:48:03 -0400

    - updates

--- Generate3rdPartyOutput.sh
+++ Generate3rdPartyOutput.sh
@@ -10,4 +10,8 @@ pwd
 THIRDPARTYROOT=`readlink -f ${SCRIPTPATH}/../3rdParty`
 echo "3rd Party INSTALL ROOT: " $THIRDPARTYROOT
 
+apt install python3-pip
+pip install requests-aws
+pip install requests
+
 python Generate3rdPartyOutput.py
