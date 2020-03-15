aa701bcc62c5a0fb9f53f7e363a9c4ca0b70fca3
David Sleeper <david.sleeper@gmail.com>
2020-03-15 10:17:28 -0400

    - admin privileges

--- Generate3rdPartyOutput.sh
+++ Generate3rdPartyOutput.sh
@@ -10,8 +10,8 @@ pwd
 THIRDPARTYROOT=`readlink -f ${SCRIPTPATH}/../3rdParty`
 echo "3rd Party INSTALL ROOT: " $THIRDPARTYROOT
 
-apt install python3-pip
-pip install requests-aws
-pip install requests
+sudo apt install python3-pip
+sudo pip install requests-aws
+sudo pip install requests
 
 python Generate3rdPartyOutput.py
