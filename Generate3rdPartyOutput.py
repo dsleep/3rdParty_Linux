ec0666d6baa4917317359b2c50d15ffd0d2bde4c
David Sleeper <david.sleeper@gmail.com>
2020-04-13 22:00:57 -0400

    - path fix

--- Generate3rdPartyOutput.py
+++ Generate3rdPartyOutput.py
@@ -55,7 +55,7 @@ def RunAndWait(ProgramLaunch, LogName=''):
 	
 	return "DONE"					
 
-ThirdPartyPath = os.path.abspath( "../")
+ThirdPartyPath = os.path.abspath( "../") + "/3rdParty"
 ThirdPartyForwardPath = ThirdPartyPath.replace( "\\", "/" )
 ScriptPath = get_script_path()
 CMakeLibInstall = "lib"
